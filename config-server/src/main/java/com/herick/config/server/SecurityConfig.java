daea665319d053ceb5cfdc4f15b286e4e056efa1
Herick <herickwill@hotmail.com>
2024-05-04 15:31:37 -0300

    feat(security): adding encryption for keys

---
+++ config-server/src/main/java/com/herick/config/server/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.herick.config.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+//ENCRIPTAR ou AO CONT.
+
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain webSecurityCustomizer(HttpSecurity http) throws Exception {
+//        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(requests -> requests
+//                        .requestMatchers(new AntPathRequestMatcher("/actuator/**"),
+//                                new AntPathRequestMatcher("/encrypt/**"),
+//                                new AntPathRequestMatcher("/encrypt"),
+//                                new AntPathRequestMatcher("/decrypt/**"))
+//                        .permitAll()
+//                        .anyRequest()
+//                        .authenticated())
+//                .httpBasic(Customizer.withDefaults());
+//        return http.build();
+//    }
+//}
+
